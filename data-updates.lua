e67b0187f76af0da342e6d77a6faa02c381f29aa
David Skrundz <david@skrundz.ca>
2020-04-26 16:56:27 -0600

    Remove dependency on the base mod

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,4 @@
-for _, v in pairs(data.raw.tile) do
+for _, v in pairs(data.raw.tile or {}) do
   if (v.decorative_removal_probability or 0) > 0 then
     v.decorative_removal_probability = 1
   end
